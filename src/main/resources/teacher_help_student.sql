cdfd9aad9dbb3ace0df766e5f8c0c9d0413b7222
taniaDruzh <tania.druzhchenko@gmail.com>
2018-05-22 20:23:33 +0300

    files

---
+++ src/main/resources/teacher_help_student.sql
@@ -0,0 +1,10 @@
+CREATE TABLE MY_TABLE (
+  idstudent INT,
+  name VARCHAR,
+  ser_name VARCHAR,
+  phone VARCHAR,
+  email VARCHAR
+);
+INSERT INTO MY_TABLE(idstudent, name, ser_name, phone, email) VALUES (7, 'ivanenkov', '', '', '');
+INSERT INTO MY_TABLE(idstudent, name, ser_name, phone, email) VALUES (15, 'ivanov', '', '', '');
+INSERT INTO MY_TABLE(idstudent, name, ser_name, phone, email) VALUES (16, 'sidorov', '', '', '');
